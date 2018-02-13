11a307f845c389a92b36c10cf870947265a5b5c9
Matthew Barber <quitesimplymatt@gmail.com>
2018-02-13 21:27:45 +0000

    create lists command functional for following < 500

--- tanew/commands/__init__.py
+++ tanew/commands/__init__.py
@@ -1,6 +1,6 @@
 from .status import *
 from .linkaccount import *
-from .linkaccount import *
 from .backup import *
+from .createlists import *
 from .unfollowall import *
 from .followall import *
