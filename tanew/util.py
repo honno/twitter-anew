9a8c2d1e8d086ff306ea3c063a7c10a6d61e4e62
Matthew Barber <quitesimplymatt@gmail.com>
2018-02-15 17:41:37 +0000

    removed fancy regex stuff for tweeperror parsing as some twitter api calls ruined it, need to think of new solution but cba right now

--- tanew/util.py
+++ tanew/util.py
@@ -1,18 +1,10 @@
+from json import loads
+
+from re import search as regex_search
+
+
 def parse_te(te):
-    from json import loads
-
-    from re import search as regex_search
-
-    string = ""
-    try:
-        json_reason = regex_search('\[(\{.+\})\]', str(te)).group(1).replace('\'', '"')
-        reason = loads(json_reason)
-        string = "Twitter API: {} (Code {})".format(reason['message'], reason['code'])
-    except AttributeError:
-        string = te
-    finally:
-        return string
-
+    return te.reason
 
 def read_friends_ids(file):
     friends_ids = []
