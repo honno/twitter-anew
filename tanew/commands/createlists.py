813fc16a04be83a7dae8e1b61dc99d6d36e4a5e2
Matthew Barber <quitesimplymatt@gmail.com>
2018-02-15 17:41:57 +0000

    user input yay

--- tanew/commands/createlists.py
+++ tanew/commands/createlists.py
@@ -1,6 +1,6 @@
 # tanew/commands/status.py
 
-from .base import Base
+from.base import Base
 
 import util
 
@@ -23,9 +23,9 @@ DEFAULT_LIST_MODE = "public"
 class CreateLists(Base):
     def run(self, auth):
 
-        list_name = DEFAULT_LIST_NAME
+        list_name = self.options['--list-name'] if self.options['--list-name'] != None else DEFAULT_LIST_NAME
         list_desc = DEFAULT_LIST_DESCRIPTION
-        list_mode = DEFAULT_LIST_MODE
+        list_mode = self.options['--list-mode'] if (self.options['--list-mode'] != None) & ((self.options['--list-mode'] == "public") | (self.options['--list-mode'] == "private")) else DEFAULT_LIST_MODE
         
         try:
             api = tweepy.API(auth)
@@ -33,7 +33,6 @@ class CreateLists(Base):
             friends_ids = api.friends_ids()
 
             friends_no = friends_ids.__len__()
-            print(friends_no)
             if friends_no > 500:
                 friends_ids_matrix = []
                 index = -1
@@ -42,7 +41,6 @@ class CreateLists(Base):
                     friends_ids_matrix.append([])
                     range_stop = friends_ids.__len__() if friends_ids.__len__() < LIST_MAX else LIST_MAX
                     for i in range(0, range_stop):
-                        print(i)
                         friends_ids_matrix[index].append(friends_ids.pop())
 
                 for i in range(0, friends_ids_matrix.__len__()):
@@ -61,9 +59,8 @@ class CreateLists(Base):
 
     def create_list(self, api, name, mode, desc, friends_ids):
         friends_ids_iter = iter(friends_ids)
-            
         twitter_list = api.create_list(name, mode, desc)
-            
+
         while True:
             try:
                 api.add_list_member(list_id=twitter_list.id, id=friends_ids_iter.__next__())
